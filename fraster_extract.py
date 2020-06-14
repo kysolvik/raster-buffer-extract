c1b3881916b76b71f2aae3f443cae76cfc8ec726
Kylen Solvik <kysolvik@gmail.com>
2020-06-14 16:00:02 -0600

    Vectorize lon calc, massive speed up

--- fraster_extract.py
+++ fraster_extract.py
@@ -4,17 +4,17 @@ import rasterio as rio
 import geopandas as gpd
 import pandas as pd
 from shapely.geometry import Point
+import time
 
 def transform_x_to_lon(center_lat, buf_x):
     latlen = 111319.5
-    lonlen = np.cos(center_lat*np.pi/180) * latlen
-    buf_x = buf_x/lonlen
+    lonlen = np.array(np.cos(center_lat*np.pi/180) * latlen)
+    buf_x = buf_x[:, np.newaxis]/(lonlen)
 
     return buf_x
 
 def transform_y_to_lat(buf_y):
-    latlen = 111319.5
-    return buf_y/latlen
+    return buf_y/111319.5
 
 def generate_points(radius=20, n=10000):
     # Takes <0.03 sec for 10,000 pts!
@@ -28,15 +28,19 @@ def get_mean(band, cell_indices, cell_counts):
     # Outdated
     return np.average(band[cell_indices[0], cell_indices[1]], weights=cell_counts)
 
-def get_all_cells(target_pts, sample_pts, ds, to_latlon=True):
+def get_all_cells(target_pts, sample_pts, ds, latlon=True):
     # This is the slowest section by far. Need to find a speed up
-    if to_latlon:
-        x = np.array(
-                [transform_x_to_lon(sample_pts[0], target_pts.y[i]) + target_pts.x[i]
-                    for i in range(target_pts.shape[0])]
-                ).flatten()
-        sample_pts[1] = transform_y_to_lat(sample_pts[1])
-        y = np.array([sample_pts[1] + y for y in target_pts.y]).flatten()
+    if latlon:
+        s = time.time()
+        x = np.swapaxes(transform_x_to_lon(target_pts.y, sample_pts[0]) + np.array(target_pts.x),0,1).flatten()
+        print(time.time() - s)
+        print(x.shape)
+
+        s = time.time()
+        temp_y_pts = transform_y_to_lat(sample_pts[1])
+        y = np.array([temp_y_pts + y for y in target_pts.y]).flatten()
+        print(time.time() - s)
+        print(y.shape)
     else:
         x = np.array([sample_pts[0] + x for x in target_pts.x]).flatten()
         y = np.array([sample_pts[1] + y for y in target_pts.y]).flatten()
@@ -57,14 +61,14 @@ def get_all_mean_max(band, cell_indices, n_sample):
     maxes = np.max(reshaped_vals, axis=1)
     return avg, maxes
 
-def get_all_vals(band, cell_indices, n_sample):
+def get_all_vals(band, cell_indices, n_sample, latlon=True):
     reshaped_vals = extract_reshape_vals(band, cell_indices, n_sample)
     return reshaped_vals
 
-def full_pt_calc(target_pt, band, ds, sample_pts, stat='mean_max'):
-    cells = get_all_cells(target_pt, sample_pts, ds)
+def full_pt_calc(target_pt, band, ds, sample_pts, stat='mean_max', latlon=True):
+    cells = get_all_cells(target_pt, sample_pts, ds, latlon=latlon)
     if stat=='mean_max':
-        get_all_mean_max(band, cells, sample_pts.shape[1])
+        out = get_all_mean_max(band, cells, sample_pts.shape[1])
     elif stat=='all':
         out = get_all_vals(band, cells, sample_pts.shape[1])
     elif stat=='count_dict':
@@ -72,13 +76,13 @@ def full_pt_calc(target_pt, band, ds, sample_pts, stat='mean_max'):
         uniq_counts = [np.unique(vals[i], return_counts=True) for i in range(vals.shape[0])]
         out = [dict(zip(u[0], u[1])) for u in uniq_counts]
     else:
-        print('Method not recognizable, returning all cell values')
         out = get_all_vals(band, cells, sample_pts.shape[1])
+        print('Method not recognizable, returning all cell values')
     return out
 
-def random_buffer(gdf_coords, ds, radius=20, n_sample=1000, stat='mean_max'):
+def random_buffer(gdf_coords, ds, radius=20, n_sample=1000, stat='mean_max', latlon=True):
     sample_pts = generate_points(radius, n_sample)
     band = ds.read(1)
-    return full_pt_calc(gdf_coords, band, ds, sample_pts, stat=stat)
+    return full_pt_calc(gdf_coords, band, ds, sample_pts, stat=stat, latlon=latlon)
         
 
